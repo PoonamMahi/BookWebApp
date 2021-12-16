7cc3a5844fe1a86ef0785a892ea26aeaeea8466f
Kainthsourav <souravkainth@outlook.com>
2021-12-16 06:44:25 +0530

    Migration - CategoriesTypeModel

--- BookWebApp/Migrations/20211216011339_CategoriesTypeModel.Designer.cs
+++ BookWebApp/Migrations/20211216011339_CategoriesTypeModel.Designer.cs
@@ -3,14 +3,16 @@ using BookWebApp.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace BookWebApp.Migrations
 {
     [DbContext(typeof(BookContext))]
-    partial class BookContextModelSnapshot : ModelSnapshot
+    [Migration("20211216011339_CategoriesTypeModel")]
+    partial class CategoriesTypeModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ BookWebApp/Migrations/20211216011339_CategoriesTypeModel.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BookWebApp.Migrations
+{
+    public partial class CategoriesTypeModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Category",
+                columns: table => new
+                {
+                    type = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NameToken = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Category", x => x.type);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "categoryTypes",
+                columns: table => new
+                {
+                    Type = table.Column<string>(nullable: false),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_categoryTypes", x => x.Type);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Category");
+
+            migrationBuilder.DropTable(
+                name: "categoryTypes");
+        }
+    }
+}
